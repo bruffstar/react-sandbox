e8a38dc3053bade6838fc5f8edfc0636d610a5a2
Brian Laughlan <info@rocketvalley.com>
2018-04-26 23:54:47 +0200

    Small fix

--- src/assets/components/pages/UsersPage.tsx
+++ src/assets/components/pages/UsersPage.tsx
@@ -8,9 +8,9 @@ class UsersPage extends React.Component<RouteComponentProps<any>, any> {
             <div>
                 <h1>All Users</h1>
                 <ul>
-                    <li><Link to="users/1">User 1</Link></li>
-                    <li><Link to="users/2">User 2</Link></li>
-                    <li><Link to="users/3">User 3</Link></li>
+                    <li><Link to="/users/1">User 1</Link></li>
+                    <li><Link to="/users/2">User 2</Link></li>
+                    <li><Link to="/users/3">User 3</Link></li>
                 </ul>
             </div>
         );
