7102dd72d155e867737d5a0cbbfc557f283b4e4f
Brian Laughlan <info@bruffstar.com>
2018-05-12 18:25:39 +0200

    How to use images in components

--- src/assets/components/pages/About.tsx
+++ src/assets/components/pages/About.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import {RouteComponentProps} from 'react-router-dom';
+import * as logo from '../../images/icon.jpg';
 
 class About extends React.Component<RouteComponentProps<any>, any> {
     render() {
         return (
             <div>
                 <h1>About</h1>
+                <img id="logo" src={logo} alt="React Sandbox"/>
                 <p>This is the about page.</p>
             </div>
         );
